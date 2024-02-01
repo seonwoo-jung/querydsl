73ca26474c68a07c4b1d76a02e63866d2db0c3c4
seonwoojung <laborlawseon@gmail.com>
2024-02-01 15:20:53 +0900

    Initial commit

---
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -0,0 +1,11 @@
+package study.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslApplication.class, args);
+	}
+}
